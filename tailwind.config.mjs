e4487526fc873dc511fb861e21dec474c07f3374
Giovanny <zerokay02@gmail.com>
2024-08-15 10:23:24 -0500

    Uploading new files

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+import animations from '@midudev/tailwind-animations'
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				"primary": "#6700EE",
+				"secondary": "#9333ea",
+				"light": "#F4F4F4",
+				"dark": "#212224",
+				"contrast": "#E29D36",
+
+				
+			},
+			backgroundImage: {
+				"iconGrident": "linear-gradient(180deg, #666666 0%, #020617 100%);"
+			},
+		},
 	},
-	plugins: [],
+	plugins: [animations],
 }
